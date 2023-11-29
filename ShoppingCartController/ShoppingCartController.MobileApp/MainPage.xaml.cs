c931c2df9053000ff71b778b6dfc09a1b2eb18a2
Ashraf Sada <ashraf.sada@outlook.com>
2023-11-29 18:46:40 +0300

    - Added MAUI base mobile apps platrfrom

---
+++ ShoppingCartController/ShoppingCartController.MobileApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoppingCartController.MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ShoppingCartController/ShoppingCartController.MobileApp/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ShoppingCartController.MobileApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Barcode { get; set; } = null!;
+        public string ProductName { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = null!;
+    }
+}
